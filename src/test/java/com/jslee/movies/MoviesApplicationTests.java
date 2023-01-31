5275a38e625b7a0da0e40bc06cf74f21b9bd5186
Jason Lee <jslee423@gmail.com>
2023-01-31 14:08:24 -0500

    initla commit, backend with mongo and spring boot

---
+++ src/test/java/com/jslee/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jslee.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
